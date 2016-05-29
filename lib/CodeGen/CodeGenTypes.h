1d558c9ea496aaf84d956783de6fb4758d1cc120
Elie Morisse <syniurge@gmail.com>
2016-05-29 18:56:28 -0300

    Add CodeGenTypes::swapTypeCache to save and restore the existing record LLVM types between modules.
    (cherry picked from commit 6d36a3cccc7e6d50e1de910042d1ef60d161fc57)

--- lib/CodeGen/CodeGenTypes.h
+++ lib/CodeGen/CodeGenTypes.h
@@ -342,6 +342,10 @@ public:
   void addRecordTypeName(const RecordDecl *RD, llvm::StructType *Ty,
                          StringRef suffix);
   
+  // CALYPSO HACK for non-separate compilation
+  void swapTypeCache(llvm::DenseMap<const Type*, CGRecordLayout *>& CGRecordLayouts,
+                llvm::DenseMap<const Type*, llvm::StructType *>& RecordDeclTypes,
+                llvm::DenseMap<const Type *, llvm::Type *>& TypeCache);
 
 public:  // These are internal details of CGT that shouldn't be used externally.
   /// ConvertRecordDeclType - Lay out a tagged decl type like struct or union.
