136c9bcbacf2fbd400e7985c03bfa7ccd9c58d2c
Elie Morisse <syniurge@gmail.com>
2016-06-02 22:30:12 -0300

    Never emit functions and global variables with internal linkage.
    Static and anonymous namespace symbols now get emitted only once instead of once per D module (Calypso was previously visiting function bodies and records to find declarations emitted with internal linkage and emit them, not only this wasn't always correct but it was a pretty big CPU time sink as well).
    
    (cherry picked from commit dbda022aa55678deb3c4eb6e2a6dcab9b0d0ee7b)

--- lib/AST/ASTContext.cpp
+++ lib/AST/ASTContext.cpp
@@ -9475,8 +9475,9 @@ QualType ASTContext::GetBuiltinType(unsigned Id,
 
 static GVALinkage basicGVALinkageForFunction(const ASTContext &Context,
                                              const FunctionDecl *FD) {
-  if (!FD->isExternallyVisible())
-    return GVA_Internal;
+    // CALYPSO
+//   if (!FD->isExternallyVisible())
+//     return GVA_Internal;
 
   // Non-user-provided functions get emitted as weak definitions with every
   // use, no matter whether they've been explicitly instantiated etc.
@@ -9588,8 +9589,9 @@ GVALinkage ASTContext::GetGVALinkageForFunction(const FunctionDecl *FD) const {
 
 static GVALinkage basicGVALinkageForVariable(const ASTContext &Context,
                                              const VarDecl *VD) {
-  if (!VD->isExternallyVisible())
-    return GVA_Internal;
+    // CALYPSO
+//   if (!VD->isExternallyVisible())
+//     return GVA_Internal;
 
   if (VD->isStaticLocal()) {
     const DeclContext *LexicalContext = VD->getParentFunctionOrMethod();
