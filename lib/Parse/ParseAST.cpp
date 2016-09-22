31b9a16746f26a99c614130c66f53035428bd7e7
Elie Morisse <syniurge@gmail.com>
2016-09-22 20:22:32 -0300

    Leak the parser in ParseAST() if Sema->OpaqueParser holds a reference to it.
    If delayed template parsing is enabled for MSVC, Sema will rely on the Parser during function instantiation.

--- lib/Parse/ParseAST.cpp
+++ lib/Parse/ParseAST.cpp
@@ -175,4 +175,8 @@ void clang::ParseAST(Sema &S, bool PrintStats, bool SkipFunctionBodies) {
     Stmt::PrintStats();
     Consumer->PrintStats();
   }
+
+  // CALYPSO HACK leak the parser, Sema will rely on it for MSVC delayed template parsing
+  if (S.OpaqueParser == &P)
+    ParseOP.release();
 }
